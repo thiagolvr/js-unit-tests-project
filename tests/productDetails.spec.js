8d96619a348ab3cd69b0ddb7cbdfc6970049b5b7
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-02-06 15:32:39 -0300

    req 6 completado

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,26 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toEqual('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails())).toBe(true);
+    // expect(Object.prototype.toString.call(productDetails())).toBe('[object Array]');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails()).toHaveLength(2)
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails()[0]).toBe('object');
+    expect(typeof productDetails()[1]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('gel', 'detergente')[0]).not.toMatchObject(productDetails('gel', 'detergente')[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails()[0].details.productId).toMatch(/123/i);
+    expect(productDetails()[1].details.productId).toMatch(/123/i);
   });
 });
+
+// Referência: método Array.isArray e ObjectArray
+// link: https://stackoverflow.com/questions/12996871/why-does-typeof-array-with-objects-return-object-and-not-array
+// Referência: regexp with toMatch
+// link: https://stackoverflow.com/questions/57970044/check-if-a-string-contains-abc-or-cde-with-jest
