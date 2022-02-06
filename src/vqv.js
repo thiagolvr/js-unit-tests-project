f88eae9fc3bbb014948f4fcfc0348030fc7b81c8
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-02-06 13:26:36 -0300

    req 3 completado

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (!name && !age) {
+    return undefined;
+  }
+
+  const info = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+  return info;
+};
 
 module.exports = vqv;
